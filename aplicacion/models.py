7106d942cffb27c04c34541d387cb7b380867bda
omvega <omvega.inf@gmail.com>
2018-06-09 19:22:15 -0400

    App proyecto, primer commit

---
+++ aplicacion/models.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    text = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
